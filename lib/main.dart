416b2487b48a4bcbbf8d297389dfd03e1cd2b9d2
sevvalyil <112305855+sevvalyil@users.noreply.github.com>
2025-10-16 14:33:40 +0300

    Add color picker page with random color selection
    Replaced the default home page with a new color picker page. The new page allows users to select a color from a dropdown, pick a random color, and displays the selected color in a styled container.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +9,114 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      //theme: ThemeData(),
+      home: colorPickerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class colorPickerPage extends StatefulWidget {
+  const colorPickerPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<colorPickerPage> createState() => _colorPickerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+// Asıl sınıf burda başlıyor
+class _colorPickerPageState extends State<colorPickerPage> {
+  void _randomColorSelect() {
+    final color = colors.keys.toList();
+    final randomColor = color[Random().nextInt(colors.length)];
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      selectedColor = randomColor;
     });
   }
 
+  void _showColorCode() {
+
+  }
+
+  Color selectedColor = Colors.blue;
+
+  final Map<Color,String> colors = {
+    Colors.red : "Red", // bu bir entrydir. key + value dan oluşur
+    Colors.black: "Black",
+    Colors.blue : "Blue",
+    Colors.pink : "Pink",
+    Colors.yellow : "Yellow",
+    Colors.green : "Green",
+    Colors.brown : "Brown",
+    Colors.purple : "Purple",
+    Colors.grey : "Grey",
+    Colors.orange:"Orange"
+  };
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Color Picker"),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Center(child: Column(
+        children: [
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                color: selectedColor,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                     color: selectedColor.withOpacity(1),
+                    blurRadius: 15,
+                    spreadRadius: 2
+                  )
+                ]
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          SizedBox(height: 20,),
+          Text("Blue"),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
+            children: [
+              DropdownButton<Color>(
+                value: selectedColor,
+                items: colors.entries.map((entry){
+                return DropdownMenuItem(
+                    value: entry.key,
+                    child: Row(
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      color: entry.key,
+                    ),
+                    SizedBox(width: 4,),
+                    Text(entry.value),
+                  ],
+                )
+                );
+              }).toList(), onChanged: (value){
+                setState(() {
+                  selectedColor = value!;
+                });
+              },),
+              ElevatedButton(onPressed: _randomColorSelect, child: Text("Random",style: TextStyle(color: Colors.black))),
+              IconButton(onPressed: _showColorCode, icon: Icon(Icons.info)),
+              IconButton(onPressed: (){}, icon: Icon(Icons.circle_outlined)),
+
+            ],
+          )
+        ],
+      ),),
     );
   }
+
+
 }
+
